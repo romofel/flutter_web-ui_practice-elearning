7cf75f14409530cf413031d6cb51bc7debe46a91
Felipe Romo <felipe@romofel.dev>
2021-07-06 12:20:50 -0500

    Scaffold the nav bar layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset('assets/images/e_logo.svg'),
+              Text('- ducation'),
+              Text('Home'),
+              Text('Categories'),
+              Text('Course Types'),
+              Container(
+                child: Row(
+                  children: [
+                    Text('Sign up'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
